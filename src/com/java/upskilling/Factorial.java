8529f41b0d3740958d4c155a47f708f338f58d27
Pratyush <76037546+Pratyush-16@users.noreply.github.com>
2023-08-30 08:01:53 +0530

    session one

--- src/com/java/upskilling/Factorial.java
+++ src/com/java/upskilling/Factorial.java
@@ -3,7 +3,7 @@ package com.java.upskilling;
 public class Factorial {
 
 
-        public sta tic void main(String[] args) {
+        public static void main(String[] args) {
             int number = 5;
             long factorial = calculateFactorial(number);
             System.out.println("Factorial of " + number + " is: " + factorial);
@@ -16,8 +16,5 @@ public class Factorial {
                 return n * calculateFactorial(n - 1);
             }
         }
-
-
-
 
 }
